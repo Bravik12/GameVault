7dde13c25feb9466b76fa2e69bb6f7370651d7e5
Adam Bravenec <adam.bravik@gmail.com>
2026-07-09 16:52:17 +0200

    Created Game data model

---
+++ GameVault/GameVault/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameVault.Models
+{
+    internal class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public GameStatus Status { get; set; }
+        public int? Rating { get; set; }
+        public double? PlaytimeHours { get; set; }
+        public string? Notes { get; set; }
+
+
+    }
+}
---
+++ GameVault/GameVault/Models/GameStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameVault.Models
+{
+    public enum GameStatus
+    {
+        NotStarted,
+        Playing,
+        Completed,
+        Dropped
+    }
+}

2abf4df47b247e33ff2e72294b8857d8d2b4c4ec
Adam Bravenec <adam.bravik@gmail.com>
2026-07-09 18:55:22 +0200

    Bind game collection to library view

--- GameVault/GameVault/Models/Game.cs
+++ GameVault/GameVault/Models/Game.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GameVault.Models
 {
-    internal class Game
+    public class Game
     {
         public int Id { get; set; }
         public string Name { get; set; }

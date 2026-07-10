2b146fd24ab5ab82a7494e7dd8aab01a4426f0b8
Adam Bravenec <adam.bravik@gmail.com>
2026-07-10 23:19:25 +0200

    feat: add json persitence for games

---
+++ GameVault/GameVault/Services/GameStorageService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameVault.Models;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+
+namespace GameVault.Services
+{
+    public class GameStorageService
+    {
+
+        private readonly string FilePath;
+
+        public void SaveGames(IEnumerable<Game> games)
+        {
+            var json = JsonSerializer.Serialize(games, new JsonSerializerOptions 
+            { 
+                WriteIndented = true 
+            });
+
+            File.WriteAllText(FilePath, json);
+        }
+
+        public List<Game> LoadGames()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return new List<Game>();
+            }
+
+            try
+            {
+                var json = File.ReadAllText(FilePath);
+
+                return JsonSerializer.Deserialize<List<Game>>(json)
+                       ?? new List<Game>();
+            }
+            catch (JsonException)
+            {
+                return new List<Game>();
+            }
+        }
+
+        public GameStorageService()
+        {
+            var folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GameVault");
+
+            Directory.CreateDirectory(folderPath);
+
+            FilePath = Path.Combine(folderPath, "games.json");
+
+        }
+
+    }
+}
